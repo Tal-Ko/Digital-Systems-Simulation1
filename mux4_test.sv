90307a1692f2bd4aed46c4fd4e4fefa2bf43518d
Tal Kogan <tal.kogan@campus.technion.ac.il>
2021-11-28 10:17:33 +0200

    stages: wet 3.3: Add mux4 initial testbench

--- mux4_test.sv
+++ mux4_test.sv
@@ -4,6 +4,41 @@ module mux4_test;
 // Put your code here
 // ------------------
 
+	logic d0, d1, d2, d3;
+	logic [1:0] sel;
+	logic z;
+
+	mux4 mux4_inst (
+		.d0(d0),
+		.d1(d1),
+		.d2(d2),
+		.d3(d3),
+		.sel(sel),
+		.z(z)
+	);
+
+	initial begin
+		d0 = 1'b0;
+		d1 = 1'b1;
+		d2 = 1'b1;
+		d3 = 1'b1;
+		sel = {1'b0, 1'b0};
+
+		// Wait for the mux4 to stablize on an initial value
+		#56
+
+		// d0 0->1
+		d0 = 1'b1;
+		#56
+
+		// d0 1->0
+		d0 = 1'b0;
+		#112
+
+		// TODO: Write the rest of the test cases
+
+		$stop;
+	end
 
 // End of your code
 
