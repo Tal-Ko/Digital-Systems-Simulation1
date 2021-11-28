9c023c345e99396b08feed0aa1552495850fb54d
Tal Kogan <tal.kogan@campus.technion.ac.il>
2021-11-28 10:17:05 +0200

    stages: wet 3.2: Add mux4to1 implementation

--- mux4.sv
+++ mux4.sv
@@ -11,6 +11,30 @@ module mux4 (
 // Put your code here
 // ------------------
 
+    // Signal declaration for inner output wires
+    logic res1, res2;
+
+    // Gate instantiations
+    mux2 mux_d0_d1_inst (
+        .d0(d0),
+        .d1(d1),
+        .sel(sel[0]),
+        .z(res1)
+    );
+
+    mux2 mux_d2_d3_inst (
+        .d0(d2),
+        .d1(d3),
+        .sel(sel[0]),
+        .z(res2)
+    );
+
+    mux2 mux_res1_res2_inst (
+        .d0(res1),
+        .d1(res2),
+        .sel(sel[1]),
+        .z(z)
+    );
 
 // End of your code
 
