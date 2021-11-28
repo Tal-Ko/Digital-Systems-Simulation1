ba063c2875975a67d22721d811e72ed1aa891cb0
Tal Kogan <tal.kogan@campus.technion.ac.il>
2021-11-28 15:30:16 +0200

    stages: wet 3.5: Add FAS testbench

--- fas_test.sv
+++ fas_test.sv
@@ -4,6 +4,35 @@ module fas_test;
 // Put your code here
 // ------------------
 
+	logic a, b, cin, a_ns;
+	logic s, cout;
+
+	fas fas_inst(
+		.s(s),
+		.cout(cout),
+		.a(a),
+		.b(b),
+		.cin(cin),
+		.a_ns(a_ns)
+	);
+
+	initial begin
+		a = 1'b0;
+		b = 1'b0;
+		cin = 1'b0;
+		a_ns = 1'b0;
+
+		// Wait for maximum Tpd in order for the system to stablize
+		#50
+
+		// b 0->1
+		b = 1'b1;
+		#100
+
+		// TODO: Write the rest of the test cases
+
+		$stop;
+	end
 
 // End of your code
 
