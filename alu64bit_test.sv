aadf0b2ee937148794366476923ffd65291a14e4
Tal Kogan <tal.kogan@campus.technion.ac.il>
2021-11-29 14:37:51 +0200

    stages: wet 3.8: Add required ALU64Bit testbench (minimal testing)

--- alu64bit_test.sv
+++ alu64bit_test.sv
@@ -4,6 +4,29 @@ module alu64bit_test;
 // Put your code here
 // ------------------
 
+	logic [63:0] a;
+	logic [63:0] b;
+	logic cin;
+	logic [1:0] op;
+	logic [63:0] s;
+	logic cout;
+
+	alu64bit alu64bit_inst (
+		.a(a), .b(b), .cin(cin), .op(op), .s(s), .cout(cout)
+	);
+
+	initial begin
+		a = {64{1'b1}};
+		b = {64{1'b0}};
+		cin = 1'b0;
+		op = {1'b1, 1'b0};
+
+		#3500
+		cin = 1'b1;
+
+		#3500
+		$stop;
+	end
 
 // End of your code
 
