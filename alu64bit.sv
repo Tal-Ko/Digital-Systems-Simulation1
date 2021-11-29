509a12e3ab9acc1586661a8b770fc6419bcc4802
Tal Kogan <tal.kogan@campus.technion.ac.il>
2021-11-29 14:37:18 +0200

    stages: wet 3.7: Add ALU64Bit implementation

--- alu64bit.sv
+++ alu64bit.sv
@@ -11,6 +11,43 @@ module alu64bit (
 // Put your code here
 // ------------------
 
+    logic [62:0] ripple_cout;
+    logic [62:0] ripple_cin_next;
+
+    alu1bit alu1bit_inst_first(
+        .s(s[0]),
+        .cout(ripple_cout[0]),
+        .a(a[0]),
+        .b(b[0]),
+        .cin(cin),
+        .op(op)
+    );
+
+    assign ripple_cin_next[0] = ripple_cout[0];
+    genvar i;
+    generate
+        for (i = 1; i < 63; i++)
+        begin
+            alu1bit alu1bit_inst(
+                .s(s[i]),
+                .cout(ripple_cout[i]),
+                .a(a[i]),
+                .b(b[i]),
+                .cin(ripple_cin_next[i - 1]),
+                .op(op)
+            );
+            assign ripple_cin_next[i] = ripple_cout[i];
+        end
+    endgenerate
+
+    alu1bit alu1bit_inst_last(
+        .s(s[63]),
+        .cout(cout),
+        .a(a[63]),
+        .b(b[63]),
+        .cin(ripple_cin_next[62]),
+        .op(op)
+    );
 
 // End of your code
 
